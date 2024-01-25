3573202a5e1d3e9d3e2f0fede19f6cffe498595e
Maziar Sojoudian <maziar@sojoudian.net>
2024-01-25 18:49:09 -0500

    update the repo

--- w3/assignment/main.go
+++ w3/assignment/main.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println(echo("Function one, created by Maziar"))
 	fmt.Println(multiply(2, 3))
+	newPrint("Function three, created by NExt person")
+}
+
+// Function one
+// created by maziar
+// this function will echo the input string
+func echo(s string) string {
+	return s
 }
 
 // Function two
@@ -11,4 +20,11 @@ func main() {
 // and return the result with int type
 func multiply(a int, b int) int {
 	return a * b
+}
+
+// Function three
+// created by next person
+// this function will print the input string to the command line
+func newPrint(s string) {
+	fmt.Println(s)
 }
