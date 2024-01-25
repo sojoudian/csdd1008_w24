712841840b998ce730e119e59c464abc6173f5b9
Maziar Sojoudian <maziar@sojoudian.net>
2024-01-25 17:44:23 -0500

    update the repo

--- w3/main.go
+++ w3/main.go
@@ -1,9 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
 	printName("Maz")
+	fmt.Println(sum(10, 20))
+	echo()
+	functionName()
+}
+
+func echo() {
+	fmt.Println("This string is printerd from echo function")
+}
+
+func sum(a int, b int) int {
+	c := a + b
+	return c
 }
 
 func printName(name string) {
 	println(name)
+}
+
+func functionName() {
+
 }
