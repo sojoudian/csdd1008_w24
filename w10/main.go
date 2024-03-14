b071f12816cc8cfe64eef60c1986e55c6dfb3d67
Maziar Sojoudian <maziar@sojoudian.net>
2024-03-14 17:54:18 -0400

    add getTorontoTime func

--- w10/main.go
+++ w10/main.go
@@ -1,11 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
 
 const worldTimeAPI = "http://worldtimeapi.org/api/timezone/America/Toronto"
 
 type TimeInfo struct{
   Datetime string `json:"datetime"`
+}
+
+
+func getTorontoTime() (string, error)  {
+  resp, err := http.Get(worldTimeAPI)
+  if err != nil {
+    return "Error retriving data", err
+  }
+  defer resp.Body.Close()
+  body, err := io.ReadAll(resp.Body)
+  if err != nil {
+    return "Error reading data", err
+  }
+  var TimeInfo TimeInfo
+  err = json.Unmarshal(body, &TimeInfo)
+  if err != nil {
+    return "Error Parsing data", err
+  }
+
+  return TimeInfo.Datetime,  nil 
 }
 
 func main() {
